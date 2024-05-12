505ec6924fecb0436c8e84c51c70dae31d57a118
LakeeshaRCL <microclip.lakeesha@gmail.com>
2024-05-12 11:38:39 +0530

    add arithmetic helper and its unit tests

---
+++ Helpers/ArithmeticHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace DotnetWebApiUnitTesting.Helpers
+{
+    public class ArithmeticHelper
+    {
+        public int Add(List<double> numbers)
+        {
+            double result = 0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            return (int)result;
+        }
+
+
+        public double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ UnitTests/ArithmeticHelperTest.cs
@@ -0,0 +1,58 @@
+﻿using DotnetWebApiUnitTesting.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class ArithmeticHelperTest
+    {
+        private ArithmeticHelper arithmeticHelper;
+
+        public ArithmeticHelperTest()
+        {
+            this.arithmeticHelper = new ArithmeticHelper();
+        }
+
+
+        [Fact(DisplayName = "Add numbers should return double type output")]
+        public void TestAddShouldReturnDouble() {
+
+            // 1. Arrange
+            List<double> numbers = new List<double> { 5.6, 10.3, -6.2};
+            
+            // 2. Act
+            var result = arithmeticHelper.Add(numbers);
+
+            // 3. Assert
+            // test return type
+            Assert.IsType<double>(result);
+
+            // test result
+            Assert.Equal(9.7, result);
+
+        }
+
+
+
+        [Theory(DisplayName = "Subtract shuould return double type output")]
+        [InlineData(8.45, 10.96)]
+        public void TestSubtractShouldReturnDouble(double number1, double number2)
+        {
+            // 1. Arrange
+
+            // 2. Act
+            var result = arithmeticHelper.Subtract(number1, number2);
+
+            // 3. Assert
+            // test return type
+            Assert.IsType<double>(result);
+
+            // test result
+            double expected = number1 - number2;
+            Assert.Equal(expected, result);
+        } 
+    }
+}
